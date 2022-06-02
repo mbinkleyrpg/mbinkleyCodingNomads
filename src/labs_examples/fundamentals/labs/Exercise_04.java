1fea171fb09a1a3b3fbc0f05af3c67c2b1f9932d
Matt Binkley <matt.binkley@redpointglobal.com>
2022-06-02 13:38:13 -0400

    Exercise 4

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -9,13 +9,29 @@ package labs_examples.fundamentals.labs;
  */
 
 public class Exercise_04 {
-git push
     public static void main(String[] args) {
 
         int i = 1;
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean itsAlmostFriday = true;
+        System.out.println("bool is: " + itsAlmostFriday);
+        char singleChar = 'f';
+        System.out.println("char is: " + singleChar);
+        byte verySmallNumber = 3;
+        System.out.println("byte is: " + verySmallNumber);
+        short longerThanByteShorterThanInt = -32_767;
+        System.out.println("short is: " + longerThanByteShorterThanInt);
+        int myInt = 2_146_483_647;
+        System.out.println("myInt is: " + myInt);
+        long superBigNumber = 9_223_372_036_854_775_806l;       //need an l at the end if bigger than an int
+        System.out.println("long is: " + superBigNumber);
+        float smallestDecimalButStillBig = 34_233_654_234.234f; //need an f at the end to not be cast to double
+        System.out.println("float is: " + smallestDecimalButStillBig);
+        double largestDecimal = 34_233_654_234_335.32;
+        System.out.println("double is: " + largestDecimal);
+
 
     }
 
