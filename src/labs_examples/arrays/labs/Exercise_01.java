a7b1f1c9f507959e942e512ad6f62fd20ab92dca
Matt Binkley <matt.binkley@redpointglobal.com>
2022-06-10 10:18:09 -0400

    arrays and loops labs added

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,6 +1,7 @@
 package labs_examples.arrays.labs;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Arrays calculator
@@ -13,5 +14,37 @@ import java.util.Arrays;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args) {
+        int sum = 0;        //will sum all of user's numbers together
+        double average = 0;     //will calculate average of user's numbers
+        int answer;             //will store user's answer to whether they want to run the program again
 
+        do{
+            // create scanner and array
+            Scanner scanner = new Scanner(System.in);
+            int[] userArray = new int[10];
+
+            // prompt user 10 times for a number and place each number into the array
+            for(int i = 0; i < 10; i++){
+                System.out.print("Enter a number between 0 and 99,999: ");
+                userArray[i] = scanner.nextInt();
+            }
+
+            //calculate the sum by iterating through the array
+            for(int j = 0; j < userArray.length; j++){
+                sum += userArray[j];
+            }
+
+            //calculate the average by dividing the sum by the length of the array
+            average = sum / userArray.length;
+
+            //print sum and average, as well as ask user to decide whether to run the program again
+            System.out.println("The sum of the numbers you entered is: " + sum);
+            System.out.println("The average of the numbers you entered is: " + average);
+            System.out.println("Do you wish to run this again? '1' for yes and '0' for no.");
+            scanner.nextLine();
+            answer = scanner.nextInt();
+        }while(answer == 1);
+
+    }
 }
--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  *  More labs_examples.arrays
  *
@@ -15,7 +17,11 @@ public class Exercise_02 {
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
         // write code here
+        //use (array.length - 1) to print dynamic changes to the array for instructions to user about how many indexes they could search
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an index to determine what value is there (a number between 0-" + (array.length - 1) + "): ");
+        int indexToReturnValueOf = scanner.nextInt();
 
-
+        System.out.println("The element at index " + indexToReturnValueOf + " is: " + array[indexToReturnValueOf]);
     }
 }
