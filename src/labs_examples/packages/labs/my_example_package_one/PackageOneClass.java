7196975078c23d5285f9ee942626c54ef967f437
mbinkleyrpg <106706732+mbinkleyrpg@users.noreply.github.com>
2022-07-07 13:26:32 -0400

    Packages (#7)
    * Package lab completed
    
    * Package lab completed

--- src/labs_examples/packages/labs/my_example_package_one/PackageOneClass.java
+++ src/labs_examples/packages/labs/my_example_package_one/PackageOneClass.java
@@ -1,2 +1,30 @@
-package labs_examples.packages.labs.my_example_package_one;public class packageOneClass {
+package labs_examples.packages.labs.my_example_package_one;
+
+import labs_examples.packages.labs.my_example_package_two.PackageTwoClass;
+import labs_examples.packages.labs.my_example_package_two.PackageTwoSubclass;
+
+public class PackageOneClass {
+     //Create at least two methods in each class, with at least one with the protected access modifier
+ //Demonstrate how the the protected access modifier restricts access from package to package
+     public static void main(String[] args) {
+            PackageTwoClass.packageTwoPublic();
+            //packageTwoClass.packageTwoProtected();      //cannot do because the current class we are in isn't a subclass of packageTwoClass
+            //packageTwoClass.packageTwoPrivate();          //cannot do because private methods/classes are only visible by classes in the same package
+
+            PackageTwoSubclass.packageTwoPublicSubclass();
+            //packageTwoSubclass.packageTwoProtectedSubclass();     //cannot do because the current class we are in isn't a subclass of packageTwoSubclass
+            //packageTwoSubclass.packageTwoPrivateSubclass();       //cannot do because private methods/classes are only visible by classes in the same package
+     }
+
+     protected static void packageOneProtected(){
+         System.out.println("I'm in package one's protected");
+     }
+
+     private static void packageOnePrivate(){
+         System.out.println("I'm in package one's private");
+     }
+
+     public static void packageOnePublic(){
+         System.out.println("I'm in package one's public");
+     }
 }
--- src/labs_examples/packages/labs/my_example_package_two/PackageTwoSubclass.java
+++ src/labs_examples/packages/labs/my_example_package_two/PackageTwoSubclass.java
@@ -1,2 +1,24 @@
-package labs_examples.packages.labs.my_example_package_two;public class packageTwoSubclass {
+package labs_examples.packages.labs.my_example_package_two;
+
+import labs_examples.packages.labs.my_example_package_one.PackageOneClass;
+
+public class PackageTwoSubclass extends PackageOneClass {
+
+    protected static void packageTwoProtectedSubclass(){
+        System.out.println("I'm in package two's protected subclass of package one");
+    }
+
+    private void packageTwoPrivateSubclass(){
+        System.out.println("I'm in package two's private subclass of package one");
+
+    }
+
+    public static void packageTwoPublicSubclass(){
+        System.out.println("I'm in package two subclass' public");
+        PackageOneClass.packageOnePublic();
+        //PackageOneClass.packageOnePrivate();        //cannot do because private methods/classes are only visible by classes in the same package
+        PackageOneClass.packageOneProtected();
+        System.out.println("Leaving package two subclass' public");
+
+    }
 }
