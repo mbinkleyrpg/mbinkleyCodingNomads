128b1981f37cb8da229dbe8bf9a0e946c27b92d6
Matt Binkley <matt.binkley@redpointglobal.com>
2022-07-07 16:12:17 -0400

    Exception Handling completed

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -7,4 +7,37 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+public class Exercise_02{
+    public static void main(String[] args) {
+        int i = 0;
+        int[] practicingEH = new int[5];
+        practicingEH[0] = 1;
+        practicingEH[1] = 1;
+        practicingEH[2] = 1;
+        practicingEH[3] = 1;
+        practicingEH[4] = 1;
+
+        divideStuff(practicingEH, i);
+        i += 1;
+        divideStuff(practicingEH, i);
+    }
+
+    public static int divideStuff(int[] practicingEH, int i){
+        try{
+            if(i == 0){
+                practicingEH[5] = 1;
+                return practicingEH[0] / practicingEH[1];
+            }
+            else{
+                return practicingEH[0] / practicingEH[4];
+            }
+
+        }catch(ArithmeticException arithmeticException){
+            System.out.println("You cannot divide by 0.");
+        }catch(ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException){
+            System.out.println("You tried to place too many elements.");
+        }
+        return -1;
+    }
+}
 
--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,25 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+import java.math.BigDecimal;
+
+public class Exercise_03{
+    public static void main(String[] args) {
+        BigDecimal a = new BigDecimal(1);
+        BigDecimal b = new BigDecimal(3);
+
+        try{
+            //comment next two lines to test finally block
+            a = a.divide(b);
+            System.out.println(a.toString());
+
+            //uncomment next two lines to test finally block
+            //a = a.multiply(b);
+            //System.out.println(a.toString());
+        }catch(ArithmeticException arithmeticException){
+            System.out.println("Dividing these two BigDecimals leads to non-terminating decimals....");
+        }finally{
+            System.out.println("This will run no matter what.");
+        }
+    }
+}
